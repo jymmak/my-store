4c4ecbaf89069ed88cd1f60474c381f95112793c
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-03-14 18:46:32 -0500

    styles

--- src/app/component/product.component.ts
+++ src/app/component/product.component.ts
@@ -1,14 +1,32 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    SimpleChanges,
+    OnInit
+} from '@angular/core';
 import { Product } from '../product.model'
 
 @Component({
     selector: 'app-product',
-    templateUrl: './product.component.html'
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
 })
-export class ProductComponent {
+export class ProductComponent implements OnChanges, OnInit {
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
-
+    constructor() {
+        console.log('1. constructor')
+    }
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('2. ngOnChanges');
+        console.log(changes);
+    }
+    ngOnInit() {
+        console.log('3. ngOnInit')
+    }
     addCart() {
         console.log("agregamos al carrito");
         this.productClicked.emit(this.product.id)
