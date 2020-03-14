2f74742e1feddf8ed70329f4dfc3d236ed927ee7
jymma karina mogollon cardoza <jkmogollon.cp@gmail.com>
2020-03-14 16:35:50 -0500

    input y output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Product} from './product.model'
+import { Product } from './product.model'
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import {Product} from './product.model'
 export class AppComponent {
   title = 'Curso de Angular';
   items = ['casa', 'pelota', 'colegio', 'silla'];
-  products: Product[] = [
+  listproduct: Product[] = [
     {
       id: '1',
       image: 'https://picsum.photos/300/200?image=244',
@@ -48,6 +48,44 @@ export class AppComponent {
     }
   ];
 
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/1.jpg',
+      title: 'Vestido Amarett',
+      price: 8000,
+      description: 'gdcgdgcdgcg'
+    },
+    {
+      id: '2',
+      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/4.jpg',
+      title: 'Vestido  Danuska',
+      price: 8000,
+      description: 'gdcgdgcdgcg'
+    },
+    {
+      id: '3',
+      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/2.jpg',
+      title: 'Casaca Brown',
+      price: 8000,
+      description: 'gdcgdgcdgcg'
+    },
+    {
+      id: '4',
+      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/3.jpg',
+      title: 'Casaca still',
+      price: 8000,
+      description: 'gdcgdgcdgcg'
+    },
+    {
+      id: '5',
+      image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/6.jpg',
+      title: 'Vestido Breska',
+      price: 8000,
+      description: 'gdcgdgcdgcg'
+    },
+
+  ];
 
   addItem() {
     this.items.push('nuevo item')
@@ -55,5 +93,11 @@ export class AppComponent {
 
   deleteItem(index: number) {
     this.items.splice(index, 1);
+  };
+
+  clickProduct(id: number) {
+    console.log('product');
+    console.log(id);
+
   }
 }
--- src/app/component/product.component.ts
+++ src/app/component/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { Product } from '../product.model'
 
 @Component({
@@ -6,42 +6,11 @@ import { Product } from '../product.model'
     templateUrl: './product.component.html'
 })
 export class ProductComponent {
-    products: Product []= [
-        {
-        id: '1',
-        image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/1.jpg',
-        title: 'Vestido Amarett',
-        price: 8000,
-        description: 'gdcgdgcdgcg'
-    },
-    {
-        id: '2',
-        image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/4.jpg',
-        title: 'Vestido  Danuska',
-        price: 8000,
-        description: 'gdcgdgcdgcg'
-    },
-    {
-        id: '3',
-        image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/2.jpg',
-        title: 'Casaca Brown',
-        price: 8000,
-        description: 'gdcgdgcdgcg'
-    },
-    {
-        id: '4',
-        image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/3.jpg',
-        title: 'Casaca still',
-        price: 8000,
-        description: 'gdcgdgcdgcg'
-    },
-    {
-        id: '5',
-        image: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/6.jpg',
-        title: 'Vestido Breska',
-        price: 8000,
-        description: 'gdcgdgcdgcg'
-    },
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
-]
+    addCart() {
+        console.log("agregamos al carrito");
+        this.productClicked.emit(this.product.id)
+    }
 }
